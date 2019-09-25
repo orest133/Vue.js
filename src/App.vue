14bb899c5aebf3d9a8bfdb0d77bc7f3f546a4dc9
isopyliuk <Ivan.Sopyliuk@altkom.pl>
2019-09-25 14:16:39 +0200

    - exercise 1 - exercise 2 - exercise 3 - exercise 4 - exercise 5
    Change-Id: If2a36042ebc0b276f41edd4224cf6b38a7f6e8a0

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Exercise1/>
+    <br/>
+    <Exercise2/>
+    <br/>
+    <Exercise3/>
+    <br/>
+    <Exercise4/>
+    <br/>
+    <Exercise5/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 
+import Exercise1 from "./components/Exercise1.vue";
+import Exercise2 from "./components/Exercise2.vue";
+import Exercise3 from "./components/Exercise3.vue";
+import Exercise4 from "./components/Exercise4.vue";
+import Exercise5 from "./components/Exercise5.vue";
 export default {
   name: "app",
   components: {
-    HelloWorld
+      Exercise1,
+      Exercise2,
+      Exercise3,
+      Exercise4,
+      Exercise5
   }
 };
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /*font-family: "Avenir", Helvetica, Arial, sans-serif;*/
+  /*-webkit-font-smoothing: antialiased;*/
+  /*-moz-osx-font-smoothing: grayscale;*/
+  /*text-align: center;*/
+  /*color: #2c3e50;*/
+  /*margin-top: 60px;*/
 }
 </style>
---
+++ src/components/Exercise2.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="exercise2">
+        <h1>Exercise 2</h1>
+        <!-- 1) Show an alert when the Button gets clicked -->
+        <div>
+            <button v-on:click="showAlert()">Show Alert</button>
+        </div>
+        <!-- 2) Listen to the "keydown" event and store the value in a data property (hint: event.target.value gives you the value) -->
+        <div>
+            <input v-on:keydown="updateValue" type="text">
+            <p>{{ value }}</p>
+        </div>
+        <!-- 3) Adjust the example from 2) to only fire if the "key down" is the ENTER key -->
+        <div>
+            <input v-on:keydown.enter="updateValue" type="text">
+            <p>{{ value }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Exercise2",
+        data: function () {
+            return {
+                value: ''
+            }
+        },
+        methods: {
+            showAlert() {
+                alert("Button is clicked!!");
+            },
+            updateValue(event) {
+                this.value = event.target.value;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
